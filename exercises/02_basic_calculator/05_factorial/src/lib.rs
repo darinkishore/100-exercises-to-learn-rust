797f00917e69fd875ab8c7e92437ca45cf728c0a
Darin Kishore <darinkishore@protonmail.com>
2024-05-21 02:54:55 +0000

    02_done

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -9,6 +9,14 @@
 // `factorial(2)` to return `2`, and so on.
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
+fn factorial (number: u32) -> u32 {
+    if number == 0 {
+        1
+    }
+    else {
+        number * factorial(number - 1)
+    }
+}
 
 #[cfg(test)]
 mod tests {
